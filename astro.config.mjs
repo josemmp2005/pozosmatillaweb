d6895ae3e3bfd591d8944022b6e4fe337845d795
josemmp2005 <josemayenp@gmail.com>
2026-04-01 12:21:13 +0200

    Add legal and privacy policy pages
    - Created 'aviso-legal.astro' for the legal notice of Pozos Matilla S.L.
    - Created 'politica-cookies.astro' detailing the cookie policy.
    - Created 'politica-privacidad.astro' outlining the privacy policy and user rights.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    devToolbar: {
+        enabled: false
+    },
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
